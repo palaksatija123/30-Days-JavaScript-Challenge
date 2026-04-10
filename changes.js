694c847982dbc8407be25f3db3dfed73674c157e
Palak Satija <122891811+palaksatija123@users.noreply.github.com>
2026-04-10 11:57:10 +0530

    Add files via upload
    Added Day 1 logic

---
+++ changes.js
@@ -0,0 +1,9 @@
+let gameName = "spidername";
+//let can be changed or assigned new value
+gameName="angrybird";
+console.log(gameName);
+
+const name = "palak";
+//cannot change const once declared assigned
+name = "plk";
+console.log(name);
---
+++ operations.js
@@ -0,0 +1,30 @@
+let score = 10;
+let bonus = 20;
+
+let TotalScore = score + bonus;
+
+let addition = 7 + 10;
+let substract = 9 - 5;
+let multiply = 9 * 5;
+let divi = 8 / 2;
+let reminder = 9 % 2;
+let expo = 2**3;
+
+
+let myscore = 110;
+myscore++;
+
+let credits = 56;
+credits --;
+
+
+//Comparision Operator
+
+let num1=3;
+let num2=3;
+let num3=6;
+
+console.log(num1==num2); //true
+console.log(num1!=num3); //true
+console.log(num1>num3);  //false
+console.log(num1<num3);  //true
