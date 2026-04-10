08880d4dfe8e0a421d28081a49bd22db9a07741e
Palak Satija <122891811+palaksatija123@users.noreply.github.com>
2026-04-10 12:05:29 +0530

    Delete changes.js

--- changes.js
+++
@@ -1,9 +0,0 @@
-let gameName = "spidername";
-//let can be changed or assigned new value
-gameName="angrybird";
-console.log(gameName);
-
-const name = "palak";
-//cannot change const once declared assigned
-name = "plk";
-console.log(name);
