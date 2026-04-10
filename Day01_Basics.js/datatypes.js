14c0721638163274c1b53f6d2606aa0e2fca9426
Palak Satija <122891811+palaksatija123@users.noreply.github.com>
2026-04-10 12:09:35 +0530

    Add files via upload

---
+++ Day01_Basics.js/datatypes.js
@@ -0,0 +1,35 @@
+console.log("hello");
+
+//comment in js singleline
+
+
+/*
+Multi line comment in js
+
+DATATYPES:
+
+1)STring
+2)Number
+3)Boolean
+4)BigInt
+5)Undefined  -- i know you exist but doesnt have defination
+6)null  --- empty (not zero)
+
+7)Object
+
+8)Symbol
+
+*/
+
+//var score = 102;
+
+let score = 105;
+let name = "palak";
+let isLoggedIn = false;
+
+//object
+let teaTypes = ["lemon tea" , "ice tea", "black tea"]
+let user = {firstname : "plk" , lastname:"satija"}
+
+let getScore = score;
+console.log(getScore);
---
+++ Day01_Basics.js/printing.js
@@ -0,0 +1,12 @@
+console.log("hello");
+console.log("chai");
+
+
+//prints output on the same line
+process.stdout.write("chai");
+process.stdout.write("chai");
+
+console.table({city:"jaipur"});
+console.warn({city:"jaipur"});
+
+
